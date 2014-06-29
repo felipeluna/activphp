cd262b64714d33f58e14d38c99df113150af5683
Felipe Luna <pancreasnou@gmail.com>
2014-06-29 20:57:51 -0300

    >> editando nome, data, interesses. -sem pedir senha.

--- edit.php
+++ edit.php
@@ -1,5 +1,8 @@
 <?php
 				require('config.php');
+				$email = 'felipe@luna.com';
+				$usuario_atual = mysql_query("select * from usuarios where email = '$email'");		
+				$row_atual = mysql_fetch_array($usuario_atual);
 
 ?>
 
@@ -10,68 +13,143 @@
 </head>
 <body>
 	<form name="cadastro" action ="edit.php" method="post">
-			<p>Editar informações;</p>
-			<div class="input-group">
-				<?php
-				$email = 'felipe@luna.com';
-				$usuario_atual = mysql_query("select * from usuarios where email = '$email'");		
-				$row_atual = mysql_fetch_array($usuario_atual);
+		<p>Editar informações;</p>
+		<div class="input-group">
+			<?php
+			
 				$nomeantigo = $row_atual['nome'];
 
 				echo "<input type='text' name='name_novo'";
-				echo "value='". $nomeantigo . "'/><br>";
+				echo "value='". $nomeantigo . "'/>
+			<br>
+			";
 
 				?>
+			<!-- <input type="e-mail" name="email" placeholder="E-mail"/>
+		</br>
+		-->
+		<label class="grupo-input">Data de Nascimento</label>
+		<?php
 
+					$datanasceu = $row_atual['data_nascimento'];
 				
-				<!-- <input type="e-mail" name="email" placeholder="E-mail"/></br> -->
-				<label class="grupo-input">Data de Nascimento</label>
-				<?php
+
+						
+					$time = strtotime($datanasceu);
+					// $anonasceu = date('Y', $time);
+					
+					$dianasceu = date('d', $time);
+				
+					// $mesnasceu = date('m', $time);
+
+					// echo $dianasceu . "-" . $mesnasceu . "-" . $anonasceu;
+
 					#GERA OS Dias do mes
-					$startyear = 1;
+				
 					echo "<select id='user_data_3i' name='dia' >";
-					for ($i=0; $i < 32; $i++){
-						echo "<option>";
-						echo $startyear++;
+					for ($i=1; $i< 32; $i++){
+
+						echo "<option ";
+						if($i < 10){
+							
+
+							$diaa = "0".$i;
+							$dianasceu2 = (string)$dianasceu;
+							if($dianasceu2 == $diaa){
+								echo "selected='selected'>";
+							}else{
+								echo ">";
+
+							}
+							echo $diaa;
+						}else{
+							$dianasceu2 = (string)$dianasceu;
+							if($dianasceu2 == $i){
+								echo "selected='selected'>";
+							}else{							
+							echo ">";
+							
+							}
+							echo $i;
+						}
+						
 						echo "</option>";
-					}
-					echo "</select>"
+						}
+					
+					echo "</select>";
 				?>
+	<select id="user_data_2i" name="mes">
+		<?php // gera meses dinamicamente + seleciona o mes em que o cara nasceu.
 
-				<select id="user_data_2i" name="mes">
-					<option value="1">Janeiro</option>
-					<option value="2">Fevereiro</option>
-					<option value="3">Março</option>
-					<option value="4">Abril</option>
-					<option value="5">Maio</option>
-					<option selected="selected" value="6">Junho</option>
-					<option value="7">Julho</option>
-					<option value="8">Agosto</option>
-					<option value="9">Setembro</option>
-					<option value="10">Outubro</option>
-					<option value="11">Novembro</option>
-					<option value="12">Dezembro</option>
-				</select>
+		$meses = Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
+		$mesnasceu = date('m', $time);
+		$mesnasceu = (string)$mesnasceu;
 
-				<?php
+		for ($i=0; $i <12 ; $i++) { 
+			if($i < 10){
+				$d = "0".$i;
+			}else{
+				$d = $i;
+			}
+			echo "<option value='" . $d . "' ";
+
+			if($mesnasceu == $d){
+
+				// vai ter selected
+				echo "selected='selected'>";
+			}else{
+
+				//naovai ter
+				echo ">";
+			}
+			//imprimir mes
+			echo $meses[$i]."</option>";
+
+
+		}
+		?>
+
+	<!-- 	<option value="01">Janeiro</option>
+		<option value="02">Fevereiro</option>
+		<option value="03">Março</option>
+		<option value="04">Abril</option>
+		<option value="05">Maio</option>
+		<option value="06">Junho</option>
+		<option value="07">Julho</option>
+		<option value="08">Agosto</option>
+		<option value="09">Setembro</option>
+		<option value="10">Outubro</option>
+		<option value="11">Novembro</option>
+		<option value="12">Dezembro</option> -->
+	</select>
+
+	<?php
 					#GERA OS ANOS para q só permita +18
+					$anonasceu = date('Y', $time);
+					$anonasceu = (string)$anonasceu;
 					$startyear = date("Y") - 18;
-					echo "<select id='user_data_1i' name='ano' >";
-					for ($i=0; $i < 80; $i++){
-						echo "<option>";
+					echo "<select id='user_data_1i' name='ano'>";
+					for ($i=0; $i< 80; $i++){
+						echo "<option ";
+						if($startyear == $anonasceu){
+							echo "selected = 'selected'>";
+						}else{
+							echo ">";
+						}
 						echo $startyear--;
 						echo "</option>";
 					}
-					echo "</select>"
+					echo "
+</select>
+"
 				?>
-			</div>
-			<br>
-			<label class="grupo-input">Atividades de interesse</label>
-			<div class="input-group">
-			<!-- PEGA INTERESSES DO BANCO  -->
-			<?php
+</div>
+<br>
+<label class="grupo-input">Atividades de interesse</label>
+<div class="input-group">
+<!-- PEGA INTERESSES DO BANCO  -->
+<?php
 				//===========
-				$email = 'felipe@luna.com';
 				$usuario_atual = mysql_query("select * from usuarios where email = '$email'");		
 				$row_atual = mysql_fetch_array($usuario_atual);
 				$id_atual = $row_atual['idusuarios'];
@@ -97,35 +175,52 @@
 					$idinteresses = $row['idinteresses'];
 					echo "<input type='checkbox' name='checkbox_novo[]' value ='$idinteresses' id='cb$idinteresses'";
 					if(in_array($idinteresses, $selectedinteresses)){
-						echo "checked='true'>";
+						echo "checked='true'>
+";
 					}else{
 						echo ">";
 					}
 
-					echo "<label for='cb$idinteresses'>";
+					echo "
+<label for='cb$idinteresses'>
+	";
  					echo $row['descricao'];
- 					echo "</label> <br />";
+ 					echo "
+</label>
+<br />
+";
 				}				
 			?>
+<!-- 	<input type="checkbox" name="checkbox[]"  value ='1' id="cb1" >
+<label for="cb1">Futebol</label>
+<br />
+<input type="checkbox" name="checkbox[]"  value ='2'>
+Voley
+<br />
+<input type="checkbox" name="checkbox[]" value ='3'>
+Basketball
+<br />
+<input type="checkbox" name="checkbox[]" value ='4'>
+Cicismo
+<br />
+<input type="checkbox" name="checkbox[]" value ='5'>
+RPG/MMO
+<br />
+<input type="checkbox" name="checkbox[]"  value ='6'>
+Jogos de Tabuleiro
+<br />
+-->
+</div>
+<br>
+<input type="password" name="pass1" placeholder="Senha de confirmação"/>
+<br>
 
-			<!-- 	<input type="checkbox" name="checkbox[]"  value ='1' id="cb1" ><label for="cb1"> Futebol</label> <br />
-				<input type="checkbox" name="checkbox[]"  value ='2'>Voley <br />
-				<input type="checkbox" name="checkbox[]" value ='3'>Basketball <br />
-				<input type="checkbox" name="checkbox[]" value ='4'>Cicismo <br />
-				<input type="checkbox" name="checkbox[]" value ='5'>RPG/MMO <br />
-				<input type="checkbox" name="checkbox[]"  value ='6'>Jogos de Tabuleiro <br /> -->
-			</div>
-			<br>
-			<input type="password" name="pass1" placeholder="Senha de confirmação"/><br>
-
-			<div class="input-group">
-				<input type="submit" name="submit_edit" value="Editar" id="editar_btn" />
-			</div>
-			</form>	
-
+<div class="input-group">
+<input type="submit" name="submit_edit" value="Editar" id="editar_btn" />
+</div>
+</form>
 
 </body>
-
 
 <?php
 		
@@ -133,8 +228,7 @@
 
 //	if($usuario){
 		//usuario logado
-		$email = 'felipe@luna.com';
-
+		
 		if(isset($_POST['submit_edit'])){
 			// novo
 			$novo_nome = $_POST['name_novo'];
@@ -164,6 +258,19 @@
 
 
 	//	---
+			// datas
+
+			
+				$ano = mysql_real_escape_string($_POST['ano']);
+				$mes = mysql_real_escape_string($_POST['mes']);
+				$dia = mysql_real_escape_string($_POST['dia']);
+			
+				$datanova = $dia . "-" . $mes . "-" . $ano;
+				$datanova = (string)date('d/m/Y', strtotime($datanova));
+
+				mysql_query("update usuarios set data_nascimento=str_to_date('$datanova', '%d/%m/%Y' ) where email = '$email'");
+
+
 
 			//deleta interesses da tabela
 			mysql_query("delete from usuarios_interesses where usuarios_idusuarios = $id_atual");
@@ -178,13 +285,9 @@
 		}
 
 
-
 	// }else{
 	// 	//usuario nao logado
 	// }
 
 
-?>
-
-
-</html>
+?></html>
