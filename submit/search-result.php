7b057b44c34661981da855b0489b43de4bd232f5
Adailson <adailsonfilho@gmail.com>
2014-07-20 19:32:49 -0300

    buscando amigo - arrumando layout

--- submit/search-result.php
+++ submit/search-result.php
@@ -1,11 +1,12 @@
 <!-- CSS -->
 <link href="styles/content-temp.css" type="text/css" rel="stylesheet" />
+<link href="styles/search-result.css" type="text/css" rel="stylesheet" />
 
 <!-- JAVASCRIPT -->
 <script src="scripts/content-temp.js" type="text/javascript"></script>
 
 <a href="" class="x"></a>
-
+<div id="search-result">
 <?php
 	if($_POST){
 
@@ -29,10 +30,10 @@
 
 				if(mysql_num_rows($sql_atividades)){
 					//imprime atividades
-					echo "<h1'>";
-					echo "Resultado de busca para:";
-					echo "</h1>";
-					echo "<h2>{$search}</h2>";
+					echo "<h2>";
+					echo "Atividades:";
+					echo "</h2>";
+					echo "<label>Resultado de busca para:<i>{$search}</i></label><br>";
 
 					//conta linhas retornadas
 					while($row=mysql_fetch_array($sql_atividades))
@@ -55,7 +56,7 @@
 				}
 
 			}else{// se busca for vazia exibe todas categorias
-				$sql_interesses= mysql_query("select idinteresse, descricao from interesses order by idinteresse") or die(mysql_error());
+				$sql_interesses= mysql_query("SELECT idinteresse, descricao FROM interesses ORDER BY idinteresse") or die(mysql_error());
 			}
 
 			
@@ -88,30 +89,36 @@
 			session_start();
 			$idusuario = $_SESSION['idusuario'];
 			
-			$sql_res= mysql_query("select idusuario, nome from usuarios where nome like '%$q%' and idusuario <> ".$idusuario." order by idusuario") or die(mysql_error());
+			$sql_res= mysql_query("SELECT u.idusuario as idusuario, u.nome as nome, c.cidade as cidade FROM usuarios u, cidades c WHERE u.idcidade = c.idcidade AND nome LIKE '%$q%' AND idusuario <> ".$idusuario." order by idusuario") or die(mysql_error());
 
 			if(mysql_num_rows($sql_res)){
 				//identifica categoria
-				echo "<div class='autocomplete-item-group'>";
-				echo "Pessoas";
-				echo "</div>";
+				echo "<h2>";
+				echo "Pessoas:";
+				echo "</h2>";
+				echo "<label>Resultado de busca para: <i>{$search}</i></label><br>";
 				
 				while($row=mysql_fetch_array($sql_res))
 				{
 					$nome =$row['nome'];
 					$id =$row['idusuario'];
-					$b_nome ='<strong>'.$q.'</strong>';
-					$final_nome = str_ireplace($q, $b_nome, $nome);
+					// $b_nome ='<strong>'.$q.'</strong>';
+					// $final_nome = str_ireplace($q, $b_nome, $nome);
 
-					echo "<div class='autocomplete-item pessoa-item' >";
-					echo "<img class='foto' src='images/user_default-35x35.png' />";
+					$cidade =utf8_encode($row['cidade']);
+
+					echo "<div class='search-item pessoa-item' >";
+					echo "<img class='foto' src='images/user_default-68x68.png' />";
+					echo "<div>";
 					echo "<input type='hidden' class='idusuario' value='$id' />";
-					echo "<a class='name'>";
-					echo $final_nome;
-					echo "</a></div>";
-
+					echo "<label class='name'><strong>{$nome}</strong></label><br>";
+					echo "<label><img src='images/local.png' >{$cidade}</label>";
+					echo "</div>";
+					echo "</div>";
+					
 				}
 			}
 		}
 	}
 ?>
+</div>
